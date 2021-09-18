93b5366693fea2043568cc297ee90cd4b7b03418
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2021-09-19 00:56:30 +0200

    Prepare project

--- app/src/main/java/com/abdallah_abdelazim/moviediscover/MoviesListFragment.kt
+++ app/src/main/java/com/abdallah_abdelazim/moviediscover/MoviesListFragment.kt
@@ -1,19 +1,15 @@
 package com.abdallah_abdelazim.moviediscover
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.abdallah_abdelazim.moviediscover.databinding.FragmentSecondBinding
+import androidx.fragment.app.Fragment
+import com.abdallah_abdelazim.moviediscover.databinding.FragmentMoviesListBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class MoviesListFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentMoviesListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +20,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentMoviesListBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +28,7 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+
     }
 
     override fun onDestroyView() {
